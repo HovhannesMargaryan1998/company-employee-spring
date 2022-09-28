ce7fd9378552cbebfef15441cc0de9050c2f26c4
User <hovo5494gmail@mail.ru>
2022-09-28 13:58:03 +0300

    Company Employees

---
+++ src/main/java/com/example/companyemployeespring/CompanyEmployeesSprigApplication.java
@@ -0,0 +1,12 @@
+package com.example.companyemployeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyEmployeesSprigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyEmployeesSprigApplication.class, args);
+    }
+}
